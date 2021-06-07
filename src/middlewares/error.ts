9bd4a85665811dd6fa690518ef32829c440b896b
liangjun <530080147@qq.com>
2021-06-07 18:05:15 +0800

    添加日志

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -2,15 +2,17 @@
  * @Author liangjun
  * @LastEditors liangjun
  * @Date 2021-01-28 17:38:24
- * @LastEditTime 2021-03-02 15:32:07
+ * @LastEditTime 2021-06-07 17:59:42
  * @Description 处理错误
  */
 import {Context, Next,Middleware} from 'koa'
- 
+import {logResponse,logError} from '../helpers/log'
+
 export default ():Middleware =>{
     return async (ctx:Context,next:Next)=>{
         try {
             await next();
+            logResponse(ctx)
         } catch (error) {
             if(error.status===401){
                 ctx.status = 401;
@@ -18,6 +20,7 @@ export default ():Middleware =>{
             }else{
                 ctx.serverError('服务器错误',error.message)
             }
+            logError(ctx,error)
         }
     }
 }
